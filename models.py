bc80bae769e74a6e96f7adcb6abc9fa5eadf49f1
niyonsaba123 <127042789+niyonsaba123@users.noreply.github.com>
2025-05-21 12:24:18 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -27,3 +28,9 @@ class Employer(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     address = db.Column(db.String(200))
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
